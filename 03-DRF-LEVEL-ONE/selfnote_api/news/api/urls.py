72423b46713ec7a049292eac3f68fdff3ffb6c66
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-05-25 06:25:23 +0900

    feat: Added Class Based View process

--- 03-DRF-LEVEL-ONE/selfnote_api/news/api/urls.py
+++ 03-DRF-LEVEL-ONE/selfnote_api/news/api/urls.py
@@ -2,11 +2,15 @@
 
 from django.urls import path
 from news.api.views import (
-    article_list_create_api_view,
-    article_detail_api_view,
+    ArticleListCreateAPIView,
+    ArticleDetailAPIView
+    # article_list_create_api_view,
+    # article_detail_api_view,
 )
 
 urlpatterns = [
-    path("articles/", article_list_create_api_view, name="article-list"),
-    path("articles/<int:pk>", article_detail_api_view, name="article-detail"),
+    path("articles/", ArticleListCreateAPIView.as_view(), name="article-list"),
+    path("articles/<int:pk>", ArticleDetailAPIView.as_view(), name="article-detail"),
+    # path("articles/", article_list_create_api_view, name="article-list"),
+    # path("articles/<int:pk>", article_detail_api_view, name="article-detail"),
 ]

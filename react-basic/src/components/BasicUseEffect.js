6e58d955a250819bc8b253e5d36e67bedabee35b
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-05-31 08:39:56 +0900

    feat: Added useEffect sample1

--- react-basic/src/components/BasicUseEffect.js
+++ react-basic/src/components/BasicUseEffect.js
@@ -2,14 +2,16 @@ import React, {useState, useEffect} from 'react'
 
 const BasicUseEffect = () => {
     const [count, setCount] = useState(0)
+    const [item, setItem] = useState(0)
 
     useEffect(() => {
         console.log("useEffect invoked");
-    }, [])
+    }, [count])
 
     return (
         <div>
             <button onClick={() => setCount(p => p+1)}>Click {count}</button>
+            <input type="text" value={item} onChange={e => setItem(e.target.value)} />
         </div>
     )
 }

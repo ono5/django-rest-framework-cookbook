90be59ddd66d5e0613404f1edb480168ff9957b8
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-05-30 17:53:55 +0900

    feat: Added duplicate useState set

--- react-basic/src/components/Basic1.js
+++ react-basic/src/components/Basic1.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Basic1 = (props) => {
     const clickHandler = () => {
         console.log('clicked');
     }
-    
+
+    const [count, setCount] = useState(0);
+
     return (
         <>
-          <button onClick={clickHandler}>Click</button>
+          <button onClick={() => {setCount(c=>c+1); setCount(c=>c+1);}}>Click {count}</button>
           <h1>Hello { props.name }</h1>  
         </>
     )

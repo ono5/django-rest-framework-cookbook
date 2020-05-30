347deec1767cef5b4149f64005c0a09d06944196
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-05-30 17:24:23 +0900

    feat: Added ModelSerializer class

--- 03-DRF-LEVEL-ONE/selfnote_api/news/api/serializers.py
+++ 03-DRF-LEVEL-ONE/selfnote_api/news/api/serializers.py
@@ -1,45 +1,19 @@
 # news/api/serializers.py
 
+from datetime import datetime
+from django.utils.timesince import timesince
 from rest_framework import serializers
 from news.models import Article
 
-from django.contrib.auth.validators import UnicodeUsernameValidator
 
-class ArticleSerializer(serializers.Serializer):
-    """Article Serializer
-    """
-    id = serializers.IntegerField(read_only=True)
-    author = serializers.CharField()
-    title = serializers.CharField()
-    description = serializers.CharField()
-    body = serializers.CharField()
-    location = serializers.CharField()
-    publication_date = serializers.DateField()
-    active = serializers.BooleanField()
-    created_at = serializers.DateTimeField(read_only=True)
-    updated_at = serializers.DateTimeField(read_only=True)
+class ArticleSerializer(serializers.ModelSerializer):
+    time_since_publication = serializers.SerializerMethodField()
 
-    def create(self, validate_data):
-        print("Article Serializer - Create")
-        return Article.objects.create(**validate_data)
-
-    def update(self, instance, validated_data):
-        instance.author = validated_data.get(
-            'author', instance.author)
-        instance.title = validated_data.get(
-            'title', instance.title)
-        instance.description = validated_data.get(
-            'description', instance.description)
-        instance.body = validated_data.get(
-            'body', instance.body)
-        instance.location = validated_data.get(
-            'location', instance.location)
-        instance.publication_date = validated_data.get(
-            'publication_date', instance.publication_date)
-        instance.active = validated_data.get(
-            'active', instance.active)
-        instance.save()
-        return instance
+    def get_time_since_publication(self, object):
+        publication_date = object.publication_date
+        now = datetime.now()
+        time_delta = timesince(publication_date, now)
+        return time_delta
 
     def validate(self, data):
         """Check that description and title are different"""
@@ -51,6 +25,58 @@ class ArticleSerializer(serializers.Serializer):
         if len(value) < 60:
             raise serializers.ValidationError("The title has to be at least 60 charactor")
         return value
+
+    class Meta:
+        model = Article
+        exclude = ("id", )
+
+
+# class ArticleSerializer(serializers.Serializer):
+#     """Article Serializer
+#     """
+#     id = serializers.IntegerField(read_only=True)
+#     author = serializers.CharField()
+#     title = serializers.CharField()
+#     description = serializers.CharField()
+#     body = serializers.CharField()
+#     location = serializers.CharField()
+#     publication_date = serializers.DateField()
+#     active = serializers.BooleanField()
+#     created_at = serializers.DateTimeField(read_only=True)
+#     updated_at = serializers.DateTimeField(read_only=True)
+#
+#     def create(self, validate_data):
+#         print("Article Serializer - Create")
+#         return Article.objects.create(**validate_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.author = validated_data.get(
+#             'author', instance.author)
+#         instance.title = validated_data.get(
+#             'title', instance.title)
+#         instance.description = validated_data.get(
+#             'description', instance.description)
+#         instance.body = validated_data.get(
+#             'body', instance.body)
+#         instance.location = validated_data.get(
+#             'location', instance.location)
+#         instance.publication_date = validated_data.get(
+#             'publication_date', instance.publication_date)
+#         instance.active = validated_data.get(
+#             'active', instance.active)
+#         instance.save()
+#         return instance
+#
+#     def validate(self, data):
+#         """Check that description and title are different"""
+#         if data["title"] == data["description"]:
+#             raise serializers.ValidationError("Title and Description must be different from one another")
+#         return data
+#
+#     def validate_title(self, value):
+#         if len(value) < 60:
+#             raise serializers.ValidationError("The title has to be at least 60 charactor")
+#         return value
 
 
 

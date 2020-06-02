f05632c65bc40113ffcdaef13d041250c1917934
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-06-02 11:16:10 +0900

    Implements with useContext + useReducer

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AppContext from './contexts/AppContext';
-import Counter from './components/Counter';
+import CompB from './components/CompB';
+
+const initialState = 0
+const reducer = (currentState, action) => {
+    switch(action) {
+        case 'add_1':
+            return currentState + 1
+        case 'multiple_3':
+            return currentState * 3
+        case 'reset':
+            return initialState
+        default:
+            return currentState
+
+    }
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
-    <AppContext.Provider value={"value from app.js"}>
+    <AppContext.Provider value={{countProvided: count, dispatchProvided: dispatch}}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Counter />
+          Count {count}
+          <CompB />
         </header>
       </div>
     </AppContext.Provider>
